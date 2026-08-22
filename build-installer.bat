ae396d7a50f24cbda7cdfcd9434763fb6c6aa9d4
CMKH1337 <Tiger030310@126.com>
2026-08-22 21:31:12 +0800

    Delete build-installer.bat

--- build-installer.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-setlocal
-set "PROJECT_DIR=%~dp01.1.0"
-if not exist "%PROJECT_DIR%\build-installer.ps1" (
-    echo Cannot find 1.1.0\build-installer.ps1
-    exit /b 1
-)
-powershell.exe -NoLogo -NoProfile -ExecutionPolicy Bypass -File "%PROJECT_DIR%\build-installer.ps1" %*
-exit /b %ERRORLEVEL%
